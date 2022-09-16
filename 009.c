83dadd400fd079ac6d9aeab74273eff05a931bc9
fastew <ljg7234@naver.com>
2022-09-16 22:15:21 +0900

    first commit

---
+++ 009.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    int j;
+    scanf("%d %d",&i,&j);
+    printf("%d\n",i+j);
+    printf("%d\n",i-j);
+    printf("%d\n",i*j);
+    printf("%d\n",i/j);
+    printf("%d\n",i%j);
+    return 0;
+
+}
