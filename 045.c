cc522a190a85e7d26afdda60fd6d7245eaaa6cab
fastew <ljg7234@naver.com>
2022-09-26 10:14:16 +0900

    first commit

---
+++ 045.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i,j,k,l,m,n;
+    scanf("%d %d %d %d %d %d",&i,&j,&k,&l,&m,&n);
+    i = 1 - i;
+    j = 1 - j;
+    k = 2 - k;
+    l = 2 - l;
+    m = 2 - m;
+    n = 8 - n;
+    printf("%d %d %d %d %d %d",i,j,k,l,m,n);
+
+}
