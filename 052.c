04f242b95ed1e85495837cc16e735cec9d84bbc8
fastew <ljg7234@naver.com>
2022-09-27 19:02:24 +0900

    first commit

---
+++ 052.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n,m,sum = 0;
+    scanf("%d\n",&n);
+    scanf("%d",&m);
+    for (int i = 0; i < n; i++){
+        int c,d;
+        scanf("%d %d",&c,&d);
+        sum += c * d;
+    
+    }
+    if (n == sum){
+        printf("Yes");
+    }
+    else {
+        printf("No");
+    }
+}
