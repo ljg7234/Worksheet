4e9b5abf2ec5077b2b613e8d164a637966d9243e
fastew <ljg7234@naver.com>
2022-09-27 16:18:48 +0900

    first commit

--- 047.c
+++ 047.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 int main(){
-    char i[50];
-    char j[50];
-    scanf("%s\n",i);
-    scanf("%s",j);
-    int a = (int) i[0] * (int) *j;
-    printf("%d",a);
+    int i,j,k;
+    scanf("%d %d %d",&i,&j,&k);
+    if (i == j == k){
+        printf("%d",10000+i * 1000);
+    }
+    else if (i ==j ){
+        printf("%d",1000+i * 100);
+    }
+    else if (j == k){
+        printf("%d",1000+ j *100);
+    }
+    else if (i == k){
+        printf("%d",1000+ i *100);
+    }
+    else {
+        if (i > j && i > k){
+            printf("%d",100 * i);
+        }
+        else if (j > k){
+            printf("%d",100 * j);
+        }
+        else {
+            printf("%d",100 * k);
+        }
+    }
 }
