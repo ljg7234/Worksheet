57c7ef09c94260028f013033993534ed089db639
fastew <ljg7234@naver.com>
2022-09-28 21:20:53 +0900

    first commit

---
+++ 056.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,d=-1,result,input,count=0;
+    scanf("%d",&input);
+    result = input;
+    while (d != result){
+        a = input / 10;
+        b = input % 10;
+        c = (a+b)%10;
+        d = (10 *b)+ c;
+        input = d;
+        count ++;
+        
+    }
+    printf("%d",count);
+
+}
+    
+    
