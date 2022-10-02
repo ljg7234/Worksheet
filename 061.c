a28e913bb49a69914cb981ae65522761158ef596
fastew <ljg7234@naver.com>
2022-10-02 22:20:26 +0900

    first commit

--- 061.c
+++ 061.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 int main(){
-    float i;
-    scanf("%f",&i);
-    float a,b,c;
-    scanf("%f %f %f",&a,&b,&c);
+    int i,max =0;
+    scanf("%d",&i);
+    int arr[i]; 
+    float sum;   
+    for (int a = 0; a < i; a++){
+        scanf("%d",&arr[a]);
+        if (max < arr[a]){
+            max = arr[a];
+        }
+    }
+    for (int a = 0; a < i; a++){
+        sum += (float)arr[a]*100/max;
+    }
     
-    if (a > b && b > c){
-        b= b/a*100;
-        c= c/a*100;
-    }
-    else if (b > a && b > c){
-        a = a/b*100;
-        c = c/b*100;
-    }
-    else if (c > a && c > b){
-        a = a/c*100;
-        b = b/c*100;
-    }
-    printf("%f",(a+b+c)/100);
+    printf("%f",sum/i);
 }
