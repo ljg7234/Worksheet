537fe646a56429ac6d9868c63cfd59612c37e936
fastew <ljg7234@naver.com>
2022-10-09 16:56:44 +0900

    first commit

--- 079.c
+++ 079.c
@@ -3,7 +3,8 @@ int main(void) {
 
     char a[2][3][7]={"dog", "cat", "horse", "pig", "duck", "bird"};
 
-    printf("%c %c\n",*(*((*a)+1)+2),*(*(*(a+1)+1)+2));
+    printf("%c %s\n",a[0][2][3] ,&a[1][2][2]);
+    printf("%c %s\n",*(*((*a)+1)+2),(*(*(a+1)+1)+2));
     return 0;
 
 }
