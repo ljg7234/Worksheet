6825fc19e8aad9a2f08dda10c97f67d4ee3b6556
fastew <ljg7234@naver.com>
2022-10-11 14:47:48 +0900

    first commit

--- 028.c
+++ 028.c
@@ -3,12 +3,12 @@ int main(){
     int check[30] = {0,};
     for (int i = 0; i <28; i++){
         int n;
-        scanf("%d",&n);
+        scanf("%d\n",&n);
         check[n-1] = 1;        
     }
     for (int j = 0; j <30; j++){
         if (check[j] == 0){
-            printf("%d",check[j]+1);
+            printf("%d\n",j+1);
         }
     }
 }
