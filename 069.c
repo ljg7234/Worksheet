71814770fd9cc7d85add6fee58b170eb58fe21ba
fastew <ljg7234@naver.com>
2022-10-20 14:15:29 +0900

    first commit

--- 069.c
+++ 069.c
@@ -3,7 +3,7 @@
 int main(void){
     int i = 100, j = 200, *p = &i, **q = &p;
     *q = &j;
-    printf("*p = %u, **q = %u, p = %p, *q = %p, q = %p", *p, **q, p, *q, q);
+    printf("*p = %u, **q = %u, p = %p, *q = %p, q = %p %p", *p, **q, p, *q, q, &j);
     return 0;
 
 }
