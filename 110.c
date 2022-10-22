f3a8cfd39df9114a2fe25a7a1862885b488ac753
fastew <ljg7234@naver.com>
2022-10-22 11:17:57 +0900

    First commit

--- 110.c
+++ 110.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+struct info{
+    char name[20]; // 이름
+    double score; // 평점 평균
+    int credit; // 취득 학점
 
-void f(int a[3]);
+};
+
+int input_data(struct info *x,int a){
+    for (int i = 0; i < a; i++){
+        scanf("%s %d %lf",x[i].name,&x[i].credit,&x[i].score);
+    }
+}
+
+int scholarship(struct info *x,int a){
+    for (int i = 0; i < a; i++){
+        if (x[i].credit >= 20 && x[i].score >= 4.3){
+            printf("%s %d %.2lf",&x[i].name,x[i].credit,x[i].score);
+        }
+    }
+}
+
 int main(void)
 
 {
-    char s[] = "I like C.";
-    char *p = "I like C.";
-    int a[3];
-    double d[5];
-    printf("%d %d %d %d\n", sizeof(s), sizeof(p), sizeof(a), sizeof(d));
-    f(a);
+    struct info students[5]; // 5명의 학생
+    input_data(students, 5); // 이름, 평점 평균, 취득 학점을 입력받는 함수
+    scholarship(students, 5);
     return 0;
 
 }
 
-void f(int a[3])
-
-{
-
-printf("f():sizeof(a) = %ld\n", sizeof(a));
-
-}
