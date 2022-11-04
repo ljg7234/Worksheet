e1c649dceb2cc431dc1953ad128ba53c7fcb3be7
fastew <ljg7234@naver.com>
2022-11-04 20:18:59 +0900

    First commit

---
+++ 126.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i,sum = 0;
+    char a[100];
+    scanf("%d\n",&i);
+    scanf("%s",a);
+    for (int q = 0; q < i; q++){
+        sum = sum + a[q] - '0';
+
+    }
+    printf("%d",sum);
+}
