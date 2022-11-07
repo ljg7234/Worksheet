0ae860c3d1cc18ad34a919ca3862326f23cc57b3
fastew <ljg7234@naver.com>
2022-11-07 10:17:59 +0900

    first commit

--- 128.c
+++ 128.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-int main(){
-    char i[3],j[3];
-    scanf("%s %s",i,j);
-    int a=0,c=0,z=0;
-    a = a + 100 * (i[2] - '0');
-    a = a + 10 * (i[1] - '0');
-    a = a + (i[0] - '0');
-    z = z + (j[0] -'0');
-    printf("%d\n",z);
-    c = c + 100 * (j[2] - '0');   
-    c = c + 10 * (j[1] - '0');
-    c = c + j[0] - '0';
-    printf("%d %d\n",a ,c);
-    printf("%c",i[2]);
 
+int main(void) {
 
+	int a, b;
+	scanf("%d %d", &a, &b);
+	a = a/100 + ((a/10)%10)*10 + ((a%100)%10)*100;
+	b = b/100 + ((b/10)%10)*10 + ((b%100)%10)*100;
+
+	if(a < b) printf("%d", b);
+	else printf("%d", a);
 }
