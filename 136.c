776ad312f79c797b9400c3772a3c75165edca0d6
fastew <ljg7234@naver.com>
2022-11-18 14:14:03 +0900

    First commit

--- 136.c
+++ 136.c
@@ -6,18 +6,19 @@ int main(){
     scanf("%d",&a);
     num = a;
     for (int i = 0; i < a; i++){
+        int alpha[26]={0,};
+        char first = '0';
         scanf("%s",b);
-        if (strlen(b) == 1){
-            continue;
-        }
         for (int j = 0; j < strlen(b); j++){
-            for (int k=0; k < strlen(b) -2; k++){
-                if (b[j] != b[j+1] && b[j] == b[j+k+2]){
-                    num--;
-                    continue;
-                }
+            if(first != b[j]){
+                first = b[j];
+                alpha[b[j]-'a'] += 1;
             }
             
+            if(alpha[b[j]-'a'] == 2){
+                num-=1;
+                break;
+            }
         }
     }
     printf("%d",num);
