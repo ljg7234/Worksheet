d8178d6b14cf75caf193a30c259f8b5218d7b846
fastew <ljg7234@naver.com>
2022-12-05 12:01:14 +0900

    First commit

--- 147.c
+++ 147.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdarg.h>
+int add(int type,...);
 int main(){
-    
+    int x;
+    x = add(3,2,4,3);
+    printf("%d",x);
+}
+
+int add(int type,...){
+    int sum = 0;
+    va_list ap;
+    va_start(ap,type);
+    for(int i = 0; i < type; i++){
+        sum += va_arg(ap,int);
+    }
+    va_end(ap);
+    return sum;
 }
