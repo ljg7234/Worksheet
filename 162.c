0a3be60dcd9277f77ba440a641dd8a66adeda9bb
fastew <ljg7234@naver.com>
2022-12-14 17:15:23 +0900

    First commit

--- 162.c
+++ 162.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct ba{
+    char name[51];
+}ba;
+int main(){
+    int i;
+    scanf("%d",&i);
+    ba* c;
+    c = (ba*)malloc(sizeof(ba)*i);
+
+
+}
