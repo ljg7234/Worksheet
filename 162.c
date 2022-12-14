9734214117d553de5199942589d83a2913adb3a7
fastew <ljg7234@naver.com>
2022-12-14 22:19:18 +0900

    First commit

--- 162.c
+++ 162.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-struct ba{
-    char name[51];
-}ba;
 int main(){
-    int i;
-    scanf("%d",&i);
-    ba* c;
-    c = (ba*)malloc(sizeof(ba)*i);
+    int num;
+    scanf("%d",&num);
+    int arr[15][15] ={0};
+    for(int i = 0; i < 15; i++){
+        arr[0][i] = i;
+    }
+    
+    
+    for(int i = 1; i < 15; i++){
+        for(int j = 1; j < 15; j++){
+            arr[i][j] = arr[i-1][j] + arr[i][j-1];
+        }
+    }
+    
+    
+    
+    for(int i = 0; i < num; i++){
+        int peo;
+        int sca;
+        scanf("%d",&peo);
+        scanf("%d",&sca);
+        printf("%d\n",arr[peo][sca]);
 
 
+        
+    }
 }
